496cb60c45fd68310938fda6dc82dd0804eeed0f
Blaga Lucian-Florin <lucian.blaga@s.unibuc.ro>
2020-11-20 10:35:14 +0000

    Small differences

--- models/trains.php
+++ models/trains.php
@@ -3,10 +3,6 @@
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/database/database_manager.php');
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/views/includes/essentials.php');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 class trains
 {
 	/*
